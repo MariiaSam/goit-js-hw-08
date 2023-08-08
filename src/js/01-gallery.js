1ca873184881fc9bff52d5df07e1a93d8e5ca2d2
MariiaSam <kontragent@ukr.net>
2023-08-08 17:43:12 +0300

    add 1 Task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from './gallery-items';
+
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class='gallery__item'>
+        <a class='gallery__link' href='${original}'>
+        <img class='gallery__image' src='${preview}' 
+       alt='${description}'/>
+        </a>
+        </li>`
+    )
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
