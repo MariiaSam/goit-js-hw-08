beda50d06dcd7f6dc5197d33f6f89e6574e366df
MariiaSam <kontragent@ukr.net>
2023-08-09 17:51:09 +0300

    corr

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-//3
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-// 4
 const onPlay = function (data) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+  localStorage.setItem("videoplayer-current-time", data.seconds);
 };
 
-//6
 player.on('timeupdate', throttle(onPlay, 1000));
 
-//5
 player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .setCurrentTime(localStorage.getItem("videoplayer-current-time"))
   .then(function (seconds) {})
   .catch(function (error) {
     switch (error.name) {
