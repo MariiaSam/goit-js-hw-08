5a9ab3406f548a38322baca15d1cf2616d8b6238
MariiaSam <kontragent@ukr.net>
2023-08-10 17:43:32 +0300

    add some part of task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('input', throttle(inputData, 500));
+form.addEventListener('submit', handlerSubmit);
+
+const object = {};
+
+function inputData(evt) {
+    object.email = email.value;
+    object.message = message.value;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(object));
+}
+
+function handlerSubmit (evt) {
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    evt.preventDefault();
+    evt.currentTarget.resert();
+
+    localStorage.removeItem('feedback-form-state');
+}
 
